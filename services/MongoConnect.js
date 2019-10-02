242424084f9c31f7b2ed7cddad664e07d3d2cfa1
luisgzz <lgpcollege@gmail.com>
2019-10-02 18:24:11 -0400

    Successfully connects to mongodb cluster

--- services/MongoConnect.js
+++ services/MongoConnect.js
@@ -1,17 +1,21 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 import Local from '../models/Local';
 import Visitor from '../models/Visitor';
 import Route from '../models/Route';
 import Pin from '../models/Pin';
 
+mongoose.set('useUnifiedTopology', true);
 
-var mongoDbUrl = 'mongodb://127.0.0.1/my_database';
-const connectDb = () => {
-  return mongoose.connect(mongoDbUrl);
-};
+var mongoDbUrl = process.env.DB_CONNECT;
+mongoose.connect(mongoDbUrl);
+
+let db = mongoose.connection
+db.once('open', function() {
+    console.log('Connected to mongo')
+});
 
 const models = { Local, Visitor, Route, Pin };
-
-export { connectDb };
 export default models;
