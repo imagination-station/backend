cf56d4928b99189873e2b3598b5cf0e405091c45
luisgzz <lgpcollege@gmail.com>
2019-10-03 12:25:11 -0400

    All test log in information is env variable now

--- test_apps/test-login.py
+++ test_apps/test-login.py
@@ -8,7 +8,7 @@ config = {
   "authDomain": "imaginationstation-3c7a7.firebaseapp.com",
   "databaseURL": "https://imaginationstation-3c7a7.firebaseio.com",
   "storageBucket": "imaginationstation-3c7a7.appspot.com",
-  "serviceAccount": "/Users/luisgzz/Documents/School/Apps&Svcs/_app/backend/keys/serviceAccountKey.json"
+  "serviceAccount": os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -16,6 +16,8 @@ firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
 
-user = auth.sign_in_with_email_and_password("test@gmail.com", "testPass123")
+test_email = os.getenv("TEST_EMAIL")
+test_pass = os.getenv("TEST_PASS")
+user = auth.sign_in_with_email_and_password(test_email, test_pass)
 
 print(user['idToken'])
