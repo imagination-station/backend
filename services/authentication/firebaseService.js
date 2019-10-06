4b27050d811c5d583de6638654270f2012d2e1bb
luisgzz <lgpcollege@gmail.com>
2019-10-06 16:15:32 -0400

    Firebase no longer needs .json file

--- services/authentication/firebaseService.js
+++ services/authentication/firebaseService.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    }),
     databaseURL: "https://imaginationstation-3c7a7.firebaseio.com"
 });
 
